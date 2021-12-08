1be930d0a28ba83cc78ee429ad212e00ba3655da
W∆L13R <76449145+ritwik-ritnov@users.noreply.github.com>
2021-12-08 10:41:49 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -501,9 +501,9 @@ async def not_chat(_, m: Message):
     else:
         buttons = [
             [
-                InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
-                InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+                InlineKeyboardButton('Main Channel', url='https://t.me/movieZcafe38X'),
+                InlineKeyboardButton('Support Group', url='https://t.me/movieZcafe38X_Req_Zone'),
             ]
             ]
-        await m.reply("<b>You can't use this bot in this group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/VCPlayerBot) below.</b>", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(buttons))
+        await m.reply("<b>Powered By, @movieZcafe38X</b>", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(buttons))
 
